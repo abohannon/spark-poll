78f187f3954d3256a1f31287a6308bd88827009b
Adam Bohannon <abo46n2@gmail.com>
2018-03-09 20:12:31 -0800

    updated poll schema

--- server/models/Poll.js
+++ server/models/Poll.js
@@ -1,14 +1,15 @@
 const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
-// const OptionsSchema = require('./Options');
+const OptionsSchema = require('./Options');
 
 const pollSchema = new Schema({
   title: { type: String, required: true },
-  // author: { type: String },
-  // dateCreated: { type: Date, default: Date.now },
-  // tags: [String],
-  // options: [OptionsSchema],
+  user: { type: Schema.Types.ObjectId, ref: 'User' },
+  dateCreated: { type: Date, default: Date.now },
+  tags: [String],
+  totalVotes: Number,
+  options: [OptionsSchema],
 });
 
 mongoose.model('polls', pollSchema);
